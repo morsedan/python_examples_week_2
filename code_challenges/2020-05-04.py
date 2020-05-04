f45f65848aac01eb4166a8be2e4697f3c2359c35
Dan Morse <djmthedev@gmail.com>
2020-05-04 08:36:20 -0700

    implemented iterative selection sort

--- code_challenges/2020-05-04.py
+++ code_challenges/2020-05-04.py
@@ -17,6 +17,15 @@ def selection_sort(arr):
     arr[0], arr[min_index] = arr[min_index], arr[0]
     return [arr[0]] + selection_sort(arr[1:])
 
+def iterative_selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i,len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
 
-array = [5, 4, 3, 2, 1]
-print(selection_sort(array))
+array = [5, 4, 74, 16, 90, 3, 2, 1]
+print(selection_sort(array.copy()))
+print(iterative_selection_sort(array.copy()))
